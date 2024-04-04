05351c14d71a898da4e1a8dbfb09af14a93ed723
João Pedro Barbosa <joppemdesu@gmail.com>
2024-04-04 13:12:02 -0400

    validações para cep, cpf e telefone

--- app/src/main/java/com/example/tela_login_projetointegrador/backendactivitys/FormCadastro.java
+++ app/src/main/java/com/example/tela_login_projetointegrador/backendactivitys/FormCadastro.java
@@ -3,12 +3,15 @@ package com.example.tela_login_projetointegrador.backendactivitys;
 import android.graphics.Color;
 import android.hardware.biometrics.BiometricPrompt;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import com.example.tela_login_projetointegrador.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -29,11 +32,16 @@ public class FormCadastro extends AppCompatActivity {
 
 
     //Objetos que serão utilizados para realização do cadastro:
-    private EditText edit_nome,editemail,edit_senha;
+    private EditText edit_nome,editemail,edit_senha, edit_cep, edit_cpf, edit_contato;
     private Button bt_cadastrar;
 
     String[] mensagens = {"Preencha todos os campos", "Cadastro realizado com sucesso!"};
     String usuarioID;
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +50,7 @@ public class FormCadastro extends AppCompatActivity {
         iniciarComponentes();
 
         //Com esse comando,o botão agora consegue "escutar" eventos de click:
+        //Ou seja, ele pode realizar o cadastro caso todas as informações estejam corretas;
         bt_cadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,26 +58,44 @@ public class FormCadastro extends AppCompatActivity {
                 String nome = edit_nome.getText().toString();
                 String email = editemail.getText().toString();
                 String senha = edit_senha.getText().toString();
+                String cpf = edit_cpf.getText().toString();
+                String cep = edit_cep.getText().toString();
+                String contato = edit_contato.getText().toString();
 
                 //Verificação se todos os campos estão preenchidos:
-                if(nome.isEmpty()  || email.isEmpty() || senha.isEmpty()){
-                    Snackbar snackbar = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_SHORT);
-                    snackbar.setBackgroundTint(Color.RED);
-                    snackbar.setTextColor(Color.BLACK);
-                    snackbar.show();
+                if(nome.isEmpty()  || email.isEmpty() || senha.isEmpty() || cpf.isEmpty() ||
+                cep.isEmpty() || contato.isEmpty()) {
+                    exibirSnackbar(mensagens[0], view);
+                }else if  (!cpf.matches("\\d{3}.\\d{3}.\\d{3}-\\d{2}")) {
+                    exibirSnackbar("CPF inválido! Formato esperado: XXX.XXX.XXX-XX", view);
+                } else if (!cep.matches("\\d{5}-\\d{3}")) {
+                    exibirSnackbar("CEP inválido! Formato esperado: XXXXX-XXX", view);
+                } else if (!contato.matches("(\\d{2}) \\d{9}")) {
+                    exibirSnackbar("Número de celular inválido! Formato esperado: (XX) XXXXXXXXX", view);
                 }else{
-
                     cadastrarusuario(view);
-
                 }
-
             }
         });
     }
+    private void exibirSnackbar(String erro, View view) {
+        Snackbar snackbar = Snackbar.make(view, erro, Snackbar.LENGTH_SHORT);
+        snackbar.setBackgroundTint(Color.RED);
+        snackbar.setTextColor(Color.BLACK);
+        snackbar.show();
+    }
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null); // Adiciona à pilha de fragmentos para permitir voltar
+        transaction.commit(); // Confirma a transação
+    }
 
+    //Método responsável por cadastrar o usuário;
     private void cadastrarusuario(View view){
         String email = editemail.getText().toString();
         String senha = edit_senha.getText().toString();
+
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
@@ -80,8 +107,9 @@ public class FormCadastro extends AppCompatActivity {
                     snackbar.setBackgroundTint(Color.GREEN);
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
-                }else{
+                    openFragment(MenuScreen.newInstance());
 
+                }else{
 
                     //Bloco de codigo para apresentar mensagens de erro devido a exceções durante Cadastro:
                     String erro;
@@ -116,11 +144,19 @@ public class FormCadastro extends AppCompatActivity {
     //Bloco de código voltado para salvar informações do usuário como nome por exemplo:
     private void SalvarDadosUsuario(){
         String nome = edit_nome.getText().toString();
+        String email = editemail.getText().toString();
+        String cpf = edit_cpf.getText().toString();
+        String cep = edit_cep.getText().toString();
+        String contato = edit_contato.getText().toString();
 
         FirebaseFirestore database = FirebaseFirestore.getInstance();
 
         Map<String,Object> usuarios = new HashMap<>();
         usuarios.put("nome",nome);
+        usuarios.put("email", email);
+        usuarios.put("cpf", cpf);
+        usuarios.put("cep", cep);
+        usuarios.put("contato", contato);
 
         //Obter o usuário atual,e conseguirá salvar o id de cada usuario
         usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -145,6 +181,9 @@ public class FormCadastro extends AppCompatActivity {
         edit_nome = findViewById(com.example.tela_login_projetointegrador.R.id.edit_nome);
         editemail = findViewById(com.example.tela_login_projetointegrador.R.id.editemail);
         edit_senha = findViewById(com.example.tela_login_projetointegrador.R.id.edit_senha);
+        edit_cpf = findViewById(R.id.edit_cpf);
+        edit_cep = findViewById(R.id.edit_cep);
+        edit_contato = findViewById(R.id.edit_contato);
         bt_cadastrar = findViewById(R.id.bt_cadastrar);
     }
 }
--- app/src/main/java/com/example/tela_login_projetointegrador/backendactivitys/MenuScreen.java
+++ app/src/main/java/com/example/tela_login_projetointegrador/backendactivitys/MenuScreen.java
@@ -32,8 +32,10 @@ public class MenuScreen extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_menu_screen, container, false);
-        // foi alterado para fragment por questão de melhores praticas, uma vez que não a necessidade de atualizar a tela toda, exemplo o menu e appbar não precisa ficar
+
+        // foi alterado para fragment por questão de melhores praticas, uma vez que não há necessidade de atualizar a tela toda, exemplo o menu e appbar não precisa ficar
         // sendo carregado a toda mudança de tela pois eles não se alteram.
+
         //https://dev.to/alexandrefreire/qual-a-diferenca-entre-activity-fragmentactivity-e-fragment-216o
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView_products);
         List<Product> productList = carregaDados();
