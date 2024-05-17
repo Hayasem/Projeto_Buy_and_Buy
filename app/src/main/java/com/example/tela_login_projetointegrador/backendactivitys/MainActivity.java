f9f74da3007e67431c1dc3a40566beb5a203895e
Barbara <barbaraffarias01@gmail.com>
2024-05-16 23:15:23 -0400

    - Ajustes para efetuar o cadastro de produto - Ajustes para exibir a lista de produto na pagina inicial

--- app/src/main/java/com/example/tela_login_projetointegrador/backendactivitys/MainActivity.java
+++ app/src/main/java/com/example/tela_login_projetointegrador/backendactivitys/MainActivity.java
@@ -22,13 +22,10 @@ import com.example.tela_login_projetointegrador.database.UserManager;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-    private FormCadastro fc;
-    private DatabaseConnection db;
     private TextView text_tela_cadastro;
     private EditText edit_email;
     private EditText edit_senha;
     private Button bt_entrar;
-    private ProgressBar progressBar;
     private UserManager userManager;
     String[] mensagens = {"Preencha todos os campos", "Login efetuado com sucesso!"};
 
@@ -49,52 +46,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(com.example.tela_login_projetointegrador.R.layout.activity_main);
 
         IniciarComponentes();
-        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        acoesClick();
+    }
 
-                Intent intent = new Intent(MainActivity.this,FormCadastro.class);
-                startActivity(intent);
-
-            }
+    private void acoesClick() {
+        text_tela_cadastro.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this,FormCadastro.class);
+            startActivity(intent);
         });
 
-        bt_entrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        bt_entrar.setOnClickListener(view -> {
 
-                String email = edit_email.getText().toString();
-                String senha = edit_senha.getText().toString();
+            String email = edit_email.getText().toString();
+            String senha = edit_senha.getText().toString();
 
-                if(email.isEmpty() || senha.isEmpty()){
-                    Snackbar snackbar = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_SHORT);
+            if(email.isEmpty() || senha.isEmpty()){
+                Snackbar snackbar = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_SHORT);
+                snackbar.setBackgroundTint(Color.RED);
+                snackbar.setTextColor(Color.BLACK);
+                snackbar.show();
+            }else{
+                boolean loginValido = userManager.compararSenha(email, senha);
+                if (loginValido){
+
+                    userManager.setUserLogado(email);
+                    Snackbar snackbar = Snackbar.make(view, mensagens[1], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.GREEN);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                    Handler handler = new Handler();
+                    handler.postDelayed(() -> {
+                        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                        startActivity(intent);
+                    }, 1000);
+                }else{
+                    Snackbar snackbar = Snackbar.make(view, "Email ou senha incorretos.", Snackbar.LENGTH_SHORT);
                     snackbar.setBackgroundTint(Color.RED);
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
-                }else{
-                    boolean loginValido = userManager.compararSenha(email, senha);
-                    if (loginValido){
-                        ContentValues values = new ContentValues();
-                        values.put("logged_in", 1);
-                        db.update("USUARIO", values, "email = ?", new String[]{email});
-                        Snackbar snackbar = Snackbar.make(view, mensagens[1], Snackbar.LENGTH_SHORT);
-                        snackbar.setBackgroundTint(Color.GREEN);
-                        snackbar.setTextColor(Color.BLACK);
-                        snackbar.show();
-                        Handler handler = new Handler();
-                        handler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
-                                startActivity(intent);
-                            }
-                        }, 1000);
-                    }else{
-                        Snackbar snackbar = Snackbar.make(view, "Email ou senha incorretos.", Snackbar.LENGTH_SHORT);
-                        snackbar.setBackgroundTint(Color.RED);
-                        snackbar.setTextColor(Color.BLACK);
-                        snackbar.show();
-                    }
                 }
             }
         });
@@ -106,6 +95,5 @@ public class MainActivity extends AppCompatActivity {
         edit_email = findViewById(com.example.tela_login_projetointegrador.R.id.edit_email);
         edit_senha = findViewById(com.example.tela_login_projetointegrador.R.id.edit_senha);
         bt_entrar = findViewById(com.example.tela_login_projetointegrador.R.id.bt_entrar);
-        progressBar = findViewById(R.id.progressBar);
     }
 }
